2fb6cd9fe45a09fa7c81f3659605857a61b7fa1d
yenruogan <zachnagourney@gmail.com>
2023-02-14 11:15:55 -0500

    click to draw

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+let color = "black";
+let click = false;
+
 document.addEventListener("DOMContentLoaded", function () {
     createBoard(16);
+
+    document.querySelector("body").addEventListener("click", function (e) {
+        if (e.target.tagName != "BUTTON") {
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if (click) {
+                draw.innerHTML = "Now You Can Draw!"
+            } else {
+                draw.innerHTML = "You're Not Allowed to Draw!"
+            }
+        }
+    })
 
     let btn_popup = document.querySelector('#popup');
     btn_popup.addEventListener("click", function () {
         let size = getSize();
         createBoard(size);
     })
-    console.log("hi")
 })
 
 function createBoard(size) {
@@ -19,9 +33,7 @@ function createBoard(size) {
 
     for (let i = 0; i < numDivs; i++) {
         let div = document.createElement('div');
-        div.addEventListener("mouseover", colorDiv() {
-
-        })
+        div.addEventListener("mouseover", colorDiv);
         board.insertAdjacentElement("beforeend", div);
     }
 }
@@ -34,7 +46,26 @@ function getSize() {
     } else if (input < 0 || input > 100) {
         message.innerHTML = "Please provide a number between 1 and 100"
     } else {
-        message.innerHTML = "Thank you!"
+        message.innerHTML = "Time to play!"
         return input;
     }
+}
+
+function colorDiv() {
+    if (click) {
+        if (color == 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = "black";
+        }
+    }
+}
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+function resetBoard() {
+    let divs = document.querySelectorAll('div')
+    divs.forEach((div) => div.style.backgroundColor = "white")
 }
